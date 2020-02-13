cea7c9d3c0ea78cfa179694d58aaafdeb8ed1214
Elise Navarro <elise.marie.navarro@gmail.com>
2020-02-13 11:40:20 -0700

    Initial Commit

---
+++ javascripts/main.js
@@ -0,0 +1,8 @@
+// Add new task items
+// Create task list
+// Update task items/lists
+// Delete all task items
+// Update task item status
+// Mark ToDo Card as Urgent
+// Delete ToDo Card
+// Filter/Search Cards by Title, Tasks, Urgency
